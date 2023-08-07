c19f85bc45ea08174c58f1a6dde33c45d9b74169
Jushkin <jushkin.saparmatov@gmail.com>
2023-08-07 15:17:31 +0500

    docker file updated

--- src/common/configs/typeorm.config.ts
+++ src/common/configs/typeorm.config.ts
@@ -1,4 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'dotenv';
 import { About } from 'src/modules/about/entities/about.entity';
 import { Admin } from 'src/modules/admin/entities/admin.entity';
 import { Applicant } from 'src/modules/applicant/entities/applicant.entity';
@@ -9,14 +10,14 @@ import { News } from 'src/modules/news/entities/news.entity';
 import { ServiceCard } from 'src/modules/service_cards/entities/service_card.entity';
 import { SocialNetwork } from 'src/modules/social_networks/entities/social_network.entity';
 import { Statistic } from 'src/modules/statistics/entities/statistic.entity';
-
+config();
 const Dbconfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 3306,
-  username: 'jushkin',
-  password: 'George.101',
-  database: 'tejamkorUz',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [
     Admin,
     Applicant,
@@ -31,8 +32,6 @@ const Dbconfig: TypeOrmModuleOptions = {
   ],
   synchronize: false,
   migrationsRun: false,
-  // logger: 'advanced-console',
-  // logging: 'all',
 };
 
 export default Dbconfig;
